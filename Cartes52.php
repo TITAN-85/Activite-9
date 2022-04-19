afa95a3d06ced8c29d05942c29ae8f844bde0b52
AlexandruCandu <candualexandru@gmail.com>
2022-04-19 17:16:10 -0400

    quel que changements

--- Cartes52.php
+++ Cartes52.php
@@ -4,9 +4,19 @@ $nums = [];
 for ($i=1; $i<14; $i++){
     $nums[] = $i;
 }
-var_dump($nums);
+var_dump($nums, "<br>");
+//print_r($nums, "<br>");
+$suit = ["♦", "♣", "♥", "♠"];
 
-$suit = ["carreau", "trefle", "coeur", "pique"]
+$deck = [];
+
+for ($i=0; $i < count($suit); $i++) {
+    for ($f=0; $f < count($nums); $f++) {
+        $deck[] = $nums.[$f] || $suit.[$i];
+
+        echo "\$Deck", "<br>", var_dump ($Deck,"<br>");
+    }
+}
 
 
 
