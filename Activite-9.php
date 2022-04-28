891ea4a44a03ec64654ddf207489631b2d48ccd5
AlexandruCandu <candualexandru@gmail.com>
2022-04-28 13:02:11 -0400

    Split 1 deck to 2 deck

--- Activite-9.php
+++ Activite-9.php
@@ -3,14 +3,30 @@
 $nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 $suit = ["♦", "♣", "♥", "♠"];
 $deck = [];
+$deck1 =[];
+$deck2 =[];
 
-for ($i=0; $i < count($suit); $i++) {
+
+for ($i=0; $i < count($suit); $i++){
     for ($j=0; $j < count($nums); $j++) {
-
         $deck[] = $nums[$j]." ".$suit[$i];
 
     }
 }
+for ($i=0; $i < 26; $i++){
+    $deck1[] = $deck[$i];
+}
+for ($i=0; $i < 26; $i++){
+    $deck2[] = $deck[$i];
+}
+for($i=0; $i < 52; $i++){
+
+}
+
+
+
+
+
 echo "\$Deck", "<br>", var_dump ($deck,"<br>");
 print_r($deck);
 
